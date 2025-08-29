9b2f50c29c935ea2ab6e585ea5241f0323e5a3fc
Tinotenda Biningu <mac@macs-MacBook-Pro.local>
2025-08-29 22:54:50 +0200

    all

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import "./env.js"; // Load environment variables first
+import { config } from 'dotenv';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
